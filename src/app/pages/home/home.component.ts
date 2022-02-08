bbbfdc9eb3bbac1dbc9ac18af247d2b6fb28fc16
pleijan <72879538+pleijan@users.noreply.github.com>
2022-02-08 18:43:02 -0500

    ajout du calcul d'heure par semestre

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,6 +21,15 @@ interface matiere{
 })
 export class HomeComponent implements OnInit {
 
+  semestre1HDC:number = 0;
+  semestre2HDC:number = 0;
+  semestre3HDC:number = 0;
+  semestre4HDC:number = 0;
+  semestre1HAD:number = 0;
+  semestre2HAD:number = 0;
+  semestre3HAD:number = 0;
+  semestre4HAD:number = 0;
+
   matieres: matiere[] = [
     {nom: 'Mathematique', listeCours:[
       {nom:"MAT1",heureDeCours:3,heureAvecDevoir:5,optionnel:false},
@@ -86,10 +95,47 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-  drop(event: CdkDragDrop<string[]>,matiere: matiere) {
-    moveItemInArray(matiere.listeCours, event.previousIndex, event.currentIndex);
-    console.log(this.matieres)
+    this.calculSemestre()
   }
 
+  drop(event: CdkDragDrop<string[]>,matiere: matiere) {
+    moveItemInArray(matiere.listeCours, event.previousIndex, event.currentIndex);
+    this.calculSemestre()
+  }
+
+  calculSemestre(){
+
+    this.semestre1HDC = 0;
+    this.semestre2HDC = 0;
+    this.semestre3HDC = 0;
+    this.semestre4HDC = 0;
+
+    this.semestre1HAD = 0;
+    this.semestre2HAD = 0;
+    this.semestre3HAD = 0;
+    this.semestre4HAD = 0;
+
+    for (let i = 0; i < this.matieres.length; i++) {
+      for (let y = 0; y < this.matieres[i].listeCours.length; y++) {
+
+        if(y==0){
+          this.semestre1HDC = this.semestre1HDC  + this.matieres[i].listeCours[y].heureDeCours;
+          this.semestre1HAD = this.semestre1HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+        }
+        if(y==1){
+          this.semestre2HDC = this.semestre2HDC  + this.matieres[i].listeCours[y].heureDeCours;
+          this.semestre2HAD = this.semestre2HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+        }
+        if(y==2){
+          this.semestre3HDC = this.semestre3HDC  + this.matieres[i].listeCours[y].heureDeCours;
+          this.semestre3HAD = this.semestre3HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+        }
+        if(y==3){
+          this.semestre4HDC = this.semestre4HDC  + this.matieres[i].listeCours[y].heureDeCours;
+          this.semestre4HAD = this.semestre4HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+        }
+      }
+    }
+
+  }
 }
