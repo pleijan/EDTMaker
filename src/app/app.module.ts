f0a6fe797d831e67eb9f1a0cafe0dcca8addf63b
pleijan <72879538+pleijan@users.noreply.github.com>
2022-02-10 16:55:21 -0500

    ajout de l'affichage des couleurs + des differentes categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,25 +13,27 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
 import {DragDropModule} from "@angular/cdk/drag-drop";
 import {MatIconModule} from "@angular/material/icon";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        NgbModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCheckboxModule,
-        MatSelectModule,
-        MatButtonModule,
-        DragDropModule,
-        MatIconModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    NgbModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatButtonModule,
+    DragDropModule,
+    MatIconModule,
+    MatTabsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,12 +5,14 @@ import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 interface cours {
   nom: string;
   heureDeCours: number;
-  heureAvecDevoir: number;
+  heureLabo: number;
+  heureDevoirs: number;
   optionnel:boolean;
 }
 
-interface matiere{
+interface categories{
   nom: string;
+  couleur:String;
   listeCours: cours[];
 }
 
@@ -30,67 +32,69 @@ export class HomeComponent implements OnInit {
   semestre3HAD:number = 0;
   semestre4HAD:number = 0;
 
-  matieres: matiere[] = [
-    {nom: 'Mathematique', listeCours:[
-      {nom:"MAT1",heureDeCours:3,heureAvecDevoir:5,optionnel:false},
-      {nom:"MAT2",heureDeCours:5,heureAvecDevoir:8,optionnel:false},
-      {nom:"MAT3",heureDeCours:4,heureAvecDevoir:6,optionnel:false},
-      {nom:"MAT4",heureDeCours:4,heureAvecDevoir:6,optionnel:false}]},
-    {nom: 'Mathematique2', listeCours:[
-      {nom:"MATF",heureDeCours:4,heureAvecDevoir:6,optionnel:true},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false}]},
-    {nom: 'Physique', listeCours:[
-      {nom:"PHY1",heureDeCours:5,heureAvecDevoir:8,optionnel:false},
-      {nom:"PHY2",heureDeCours:4,heureAvecDevoir:6,optionnel:false},
-      {nom:"PHY3",heureDeCours:5,heureAvecDevoir:8,optionnel:false},
-      {nom:"PHYF",heureDeCours:4,heureAvecDevoir:6,optionnel:true}]},
-    {nom: 'Physique2', listeCours:[
-        {nom:"PHYF",heureDeCours:4,heureAvecDevoir:6,optionnel:true},
-        {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-        {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-        {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false}]},
-    {nom: 'Chimie', listeCours:[
-      {nom:"CHI1",heureDeCours:5,heureAvecDevoir:8,optionnel:false},
-      {nom:"CHI2",heureDeCours:4,heureAvecDevoir:6,optionnel:false},
-      {nom:"CHIF",heureDeCours:5,heureAvecDevoir:8,optionnel:true},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
-    {nom: 'Biologie', listeCours:[
-      {nom:"BIO1",heureDeCours:4,heureAvecDevoir:6,optionnel:false},
-      {nom:"BIO2",heureDeCours:3,heureAvecDevoir:5,optionnel:false},
-      {nom:"BIOF",heureDeCours:4,heureAvecDevoir:6,optionnel:true},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
-    {nom: 'Informatique', listeCours:[
-      {nom:"INF1",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:"INTC",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
-    {nom: 'Francais', listeCours:[
-      {nom:"FRA1",heureDeCours:4,heureAvecDevoir:7,optionnel:false},
-      {nom:"FRA2",heureDeCours:4,heureAvecDevoir:7,optionnel:false},
-      {nom:"FRA3",heureDeCours:4,heureAvecDevoir:8,optionnel:false},
-      {nom:"FRA4",heureDeCours:4,heureAvecDevoir:6,optionnel:false}]},
-    {nom: 'Anglais', listeCours:[
-      {nom:"ANG1",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:"ANG2",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
-    {nom: 'Philosophie', listeCours:[
-      {nom:"PHI1",heureDeCours:4,heureAvecDevoir:7,optionnel:false},
-      {nom:"PHI2",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:"PHI3",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
-    {nom: 'Complementaire', listeCours:[
-      {nom:"FC",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:"FC",heureDeCours:3,heureAvecDevoir:6,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
-    {nom: 'Education physique', listeCours:[
-      {nom:"EDU1",heureDeCours:2,heureAvecDevoir:3,optionnel:false},
-      {nom:"EDU2",heureDeCours:2,heureAvecDevoir:3,optionnel:false},
-      {nom:"EDU3",heureDeCours:2,heureAvecDevoir:3,optionnel:false},
-      {nom:" ",heureDeCours:0,heureAvecDevoir:0,optionnel:false},]},
+  specifique: categories[] = [
+    {nom: 'Mathematique',couleur:"#FFFFFF", listeCours:[
+      {nom:"MAT1",heureDeCours:3,heureLabo:5,heureDevoirs:2,optionnel:false},
+      {nom:"MAT2",heureDeCours:5,heureLabo:8,heureDevoirs:2,optionnel:false},
+      {nom:"MAT3",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:false},
+      {nom:"MAT4",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:false}]},
+    {nom: 'Mathematique2',couleur:"#FFFFFF", listeCours:[
+      {nom:"MATF",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:true},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false}]},
+    {nom: 'Physique',couleur:"#FFFFFF", listeCours:[
+      {nom:"PHY1",heureDeCours:5,heureLabo:8,heureDevoirs:2,optionnel:false},
+      {nom:"PHY2",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:false},
+      {nom:"PHY3",heureDeCours:5,heureLabo:8,heureDevoirs:2,optionnel:false},
+      {nom:"PHYF",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:true}]},
+    {nom: 'Physique2',couleur:"#FFFFFF", listeCours:[
+        {nom:"PHYF",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:true},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false}]},
+    {nom: 'Chimie',couleur:"#FFFFFF", listeCours:[
+      {nom:"CHI1",heureDeCours:5,heureLabo:8,heureDevoirs:2,optionnel:false},
+      {nom:"CHI2",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:false},
+      {nom:"CHIF",heureDeCours:5,heureLabo:8,heureDevoirs:2,optionnel:true},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
+    {nom: 'Biologie',couleur:"#FFFFFF", listeCours:[
+      {nom:"BIO1",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:false},
+      {nom:"BIO2",heureDeCours:3,heureLabo:5,heureDevoirs:2,optionnel:false},
+      {nom:"BIOF",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:true},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
+    {nom: 'Informatique',couleur:"#FFFFFF", listeCours:[
+      {nom:"INF1",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+      {nom:"INTC",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+      {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
+  ];
+  general:categories[]=[
+    {nom: 'Francais',couleur:"#FFFFFF", listeCours:[
+        {nom:"FRA1",heureDeCours:4,heureLabo:7,heureDevoirs:2,optionnel:false},
+        {nom:"FRA2",heureDeCours:4,heureLabo:7,heureDevoirs:2,optionnel:false},
+        {nom:"FRA3",heureDeCours:4,heureLabo:8,heureDevoirs:2,optionnel:false},
+        {nom:"FRA4",heureDeCours:4,heureLabo:6,heureDevoirs:2,optionnel:false}]},
+    {nom: 'Anglais',couleur:"#FFFFFF", listeCours:[
+        {nom:"ANG1",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+        {nom:"ANG2",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
+    {nom: 'Philosophie',couleur:"#FFFFFF", listeCours:[
+        {nom:"PHI1",heureDeCours:4,heureLabo:7,heureDevoirs:2,optionnel:false},
+        {nom:"PHI2",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+        {nom:"PHI3",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
+    {nom: 'Complementaire',couleur:"#FFFFFF", listeCours:[
+        {nom:"FC",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+        {nom:"FC",heureDeCours:3,heureLabo:6,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
+    {nom: 'Education physique',couleur:"#FFFFFF", listeCours:[
+        {nom:"EDU1",heureDeCours:2,heureLabo:3,heureDevoirs:2,optionnel:false},
+        {nom:"EDU2",heureDeCours:2,heureLabo:3,heureDevoirs:2,optionnel:false},
+        {nom:"EDU3",heureDeCours:2,heureLabo:3,heureDevoirs:2,optionnel:false},
+        {nom:" ",heureDeCours:0,heureLabo:0,heureDevoirs:2,optionnel:false},]},
   ];
   constructor() { }
 
@@ -98,7 +102,7 @@ export class HomeComponent implements OnInit {
     this.calculSemestre()
   }
 
-  drop(event: CdkDragDrop<string[]>,matiere: matiere) {
+  drop(event: CdkDragDrop<string[]>,matiere: categories) {
     moveItemInArray(matiere.listeCours, event.previousIndex, event.currentIndex);
     this.calculSemestre()
   }
@@ -115,24 +119,46 @@ export class HomeComponent implements OnInit {
     this.semestre3HAD = 0;
     this.semestre4HAD = 0;
 
-    for (let i = 0; i < this.matieres.length; i++) {
-      for (let y = 0; y < this.matieres[i].listeCours.length; y++) {
+    for (let i = 0; i < this.general.length; i++) {
+      for (let y = 0; y < this.general[i].listeCours.length; y++) {
 
         if(y==0){
-          this.semestre1HDC = this.semestre1HDC  + this.matieres[i].listeCours[y].heureDeCours;
-          this.semestre1HAD = this.semestre1HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+          this.semestre1HDC = this.semestre1HDC  + this.general[i].listeCours[y].heureDeCours;
+          this.semestre1HAD = this.semestre1HAD  + this.general[i].listeCours[y].heureLabo;
         }
         if(y==1){
-          this.semestre2HDC = this.semestre2HDC  + this.matieres[i].listeCours[y].heureDeCours;
-          this.semestre2HAD = this.semestre2HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+          this.semestre2HDC = this.semestre2HDC  + this.general[i].listeCours[y].heureDeCours;
+          this.semestre2HAD = this.semestre2HAD  + this.general[i].listeCours[y].heureLabo;
         }
         if(y==2){
-          this.semestre3HDC = this.semestre3HDC  + this.matieres[i].listeCours[y].heureDeCours;
-          this.semestre3HAD = this.semestre3HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+          this.semestre3HDC = this.semestre3HDC  + this.general[i].listeCours[y].heureDeCours;
+          this.semestre3HAD = this.semestre3HAD  + this.general[i].listeCours[y].heureLabo;
         }
         if(y==3){
-          this.semestre4HDC = this.semestre4HDC  + this.matieres[i].listeCours[y].heureDeCours;
-          this.semestre4HAD = this.semestre4HAD  + this.matieres[i].listeCours[y].heureAvecDevoir;
+          this.semestre4HDC = this.semestre4HDC  + this.general[i].listeCours[y].heureDeCours;
+          this.semestre4HAD = this.semestre4HAD  + this.general[i].listeCours[y].heureLabo;
+        }
+      }
+    }
+
+    for (let i = 0; i < this.specifique.length; i++) {
+      for (let y = 0; y < this.specifique[i].listeCours.length; y++) {
+
+        if(y==0){
+          this.semestre1HDC = this.semestre1HDC  + this.specifique[i].listeCours[y].heureDeCours;
+          this.semestre1HAD = this.semestre1HAD  + this.specifique[i].listeCours[y].heureLabo;
+        }
+        if(y==1){
+          this.semestre2HDC = this.semestre2HDC  + this.specifique[i].listeCours[y].heureDeCours;
+          this.semestre2HAD = this.semestre2HAD  + this.specifique[i].listeCours[y].heureLabo;
+        }
+        if(y==2){
+          this.semestre3HDC = this.semestre3HDC  + this.specifique[i].listeCours[y].heureDeCours;
+          this.semestre3HAD = this.semestre3HAD  + this.specifique[i].listeCours[y].heureLabo;
+        }
+        if(y==3){
+          this.semestre4HDC = this.semestre4HDC  + this.specifique[i].listeCours[y].heureDeCours;
+          this.semestre4HAD = this.semestre4HAD  + this.specifique[i].listeCours[y].heureLabo;
         }
       }
     }
